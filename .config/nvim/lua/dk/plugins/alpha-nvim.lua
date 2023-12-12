1458d1250b25028fa3f605b3945179c525738f85
Djamseed Khodabocus <djamseed.khodabocus@checkout.com>
2023-12-12 11:20:32 +0400

    fix alpha keymap

--- .config/nvim/lua/dk/plugins/alpha-nvim.lua
+++ .config/nvim/lua/dk/plugins/alpha-nvim.lua
@@ -19,7 +19,7 @@ return {
 
         -- Set menu
         dashboard.section.buttons.val = {
-            dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+            dashboard.button("e", "  > New File", "<cmd>ene <CR>"),
             dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Ex<CR>"),
             dashboard.button("SPC sf", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
             dashboard.button("SPC sg", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
